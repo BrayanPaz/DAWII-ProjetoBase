8900df799e796b745b059455308ca6b440b536c9
BrayanPaz <brayanpaz.bg016@academico.ifsul.edu.br>
2025-08-21 08:45:51 -0300

    views/aluno

---
+++ controllers/AlunoController.js
@@ -0,0 +1,22 @@
+import Aluno from '../models/Aluno.js';
+
+export default class AlunoController{
+    constructor(caminhoBase='aluno/'){
+        this.caminhoBase = caminhoBase
+        this.openAdd = async (req,res)=>{
+            res.render(this.caminhoBase+'add')
+        }
+        this.add = async(req, res)=>{
+            //cria aluno
+            await Aluno.create({
+                nome: req.body.nome,
+                matricula: req.body.matricula,
+            })
+            res.redirect('/'+this.caminhoBase+'add')
+        }
+        this.list = async (req, res) => {
+            const resultado = await Aluno.find({})
+            res.render(this.caminhoBase +'lst',  {Alunos: resultado})
+        }
+    }
+}
---
+++ routes/AlunoRoute.js
@@ -0,0 +1,10 @@
+import Express from 'express'
+const router = express.Router()
+import AlunoController from '../controllers/AlunoController.js'
+const controller = new AlunoController()
+
+const caminhobase = 'aluno/'
+
+router.get(`/${caminhobase}add`, controller.openAdd)
+router.post(`/${caminhobase}add`, controller.add)
+router.get(`/${caminhobase}add`, controller.list)
